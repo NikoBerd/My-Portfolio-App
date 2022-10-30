ac234cb0e1080c6118417cab069442c1c2824d36
Niko Berdzenishvili <nikolozisia@gmail.com>
2022-10-30 11:59:44 +0400

    Initial Commit

---
+++ NikoCard/NikoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikoCardApp.swift
+//  NikoCard
+//
+//  Created by Niko on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct NikoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
